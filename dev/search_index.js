14b7d689d4929aa2a90291a3f79aaa266aac3c5a
Documenter.jl <documenter@juliadocs.github.io>
2025-08-13 09:18:03 +0000

    build based on 3b148ee

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#APop","page":"Home","title":"APop","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for APop.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
