a36b98e9117a7e743fb7994765ff07eca7a31674
Peter Arndt <pfarndt@users.noreply.github.com>
2025-08-14 15:36:17 +0200

    Remove unused AbstractRateDistribution and UniformRate definitions from Genome module

--- src/Genome/genome.jl
+++ src/Genome/genome.jl
@@ -1,23 +1,5 @@
 
-export Genome, UniformRate, rate, recombination, mutation
-
-
-abstract type AbstractRateDistribution end
-
-
-"""
-    UniformRate(rate::Float64)
-
-    A uniform rate distribution for genetic processes.
-"""
-struct UniformRate <: AbstractRateDistribution
-    rate::Float64
-end
-
-rate(r::UniformRate) = r.rate
-
-Base.show(io::IO, r::UniformRate) = print(io, "Uniform(rate=$(r.rate))")
-
+export Genome, rate, recombination, mutation
 
 
 
