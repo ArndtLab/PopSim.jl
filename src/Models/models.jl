5f53c6fd658aafde532ee5601de7eb4d7ab24aba
Peter Arndt <pfarndt@users.noreply.github.com>
2025-09-26 19:19:20 +0200

    Export get_ARGsegments and get_IBDsegments in models.jl for improved accessibility

--- src/Models/models.jl
+++ src/Models/models.jl
@@ -1,8 +1,10 @@
 abstract type AbstractEvolutionaryModel end
 
-export AbstractEvolutionaryModel, sim_ancestry
+export AbstractEvolutionaryModel, sim_ancestry, get_ARGsegments, get_IBDsegments
 
 function sim_ancestry end
+# function get_ARGsegments end
+# function get_IBDsegments end
 
 
 include("SMC/SMC.jl")
